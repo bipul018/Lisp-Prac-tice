0e398006495985bb480bfd36aa63f21b6fee92d6
Bipul Neupane <bipuls2073@gmail.com>
2022-06-06 14:17:43 +0545

    First edit

--- dfa.lisp
+++ dfa.lisp
@@ -3,6 +3,30 @@
 (defvar *dfa-n-state*)
 (defvar *dfa-n-char*)
 
+(print "Enter number of alphabets involved : ")
+(setf *dfa-n-char* (read))
+(print "Enter number of states involved: ")
+(setf *dfa-n-state* (read))
+(print "Now enter the transition function: ")
+(terpri)
+
+(defvar *i* 0)
+(defvar *j* 0)
+
+(block out-block 	(loop 
+	(when (not (< *i* *dfa-n-state*) ) (return) )
+	(format t "Enter fxns for state q~a~%" *i*)
+	( block in-block (loop
+		(when (not (< *j* *dfa-n-char*) ) (return))
+		(format t "Enter fxn for q~a(~a) : " *i* *j*)
+		(
+		)
+	))
+
+	
+)
+
+
 
 (setf *dfa-table* (read))
 
