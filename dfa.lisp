92b4e1bdc873c4934730ff82a022c0116f8b4316
Bipul Neupane <bipuls2073@gmail.com>
2022-06-08 10:01:49 +0545

    list of list read

--- dfa.lisp
+++ dfa.lisp
@@ -16,19 +16,27 @@
 (block out-block 	(loop 
 	(when (not (< *i* *dfa-n-state*) ) (return) )
 	(format t "Enter fxns for state q~a~%" *i*)
+	(defvar state-list )
+	(setf state-list '())
+	(setf *j* 0)
 	( block in-block (loop
 		(when (not (< *j* *dfa-n-char*) ) (return))
 		(format t "Enter fxn for q~a(~a) : " *i* *j*)
-		(
+		(defvar next-state )
+		(setf next-state (read))
+		(setf next-state (if (listp next-state) (next-state) (list next-state)))
+		(setf state-list (append state-list next-state))
+		
+		(setf *j* (+ *j* 1))
 		)
-	))
+	)
+	(setf *dfa-table* (append *dfa-table* `(,state-list)))
+	(setf *i* (+ *i* 1))
+	)
 
 	
 )
 
 
 
-(setf *dfa-table* (read))
-
-
-(format t "~a~%" (car *dfa-table*))
+(format t "~a~%" *dfa-table*)
